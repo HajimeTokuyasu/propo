2aa4552c341f6bded29cffc0e25f31456edb07f6
HajimeTokuyasu <p111026h@mail.kyutech.jp>
2016-04-29 20:45:12 +0900

    Debbuging_complete

--- propo.c
+++ propo.c
@@ -30,17 +30,17 @@ int main (int argc, char *argv[]){
    int fd_joy, fd_sock;
    struct js_event event;
    if (argc != 2){
-      puts("PUT_IPofPi\n");
+      puts("PUT_IPofPi");
       return -1;
    }
-   puts("proc_getting_started\n");
+   puts("proc_getting_started");
    struct sockaddr_in svaddr;
    socklen_t len;
    len = sizeof(struct sockaddr_in);
 
    fd_sock = socket(AF_INET,SOCK_DGRAM,0);
    if (fd_sock == -1){
-      puts("ERROR_socket_missing\n");
+      puts("ERROR_socket_missing");
       return -1;
    }
 
@@ -51,65 +51,90 @@ int main (int argc, char *argv[]){
    svaddr.sin_port = htons(PORT);
 
    if (inet_pton(AF_INET, argv[1], &svaddr.sin_addr) <= 0){
-      printf("ERROR_CANNOT_PACKADDR\n");
+      puts("ERROR_CANNOT_PACKADDR");
       return -1; 
    }
 
    fd_joy = open("/dev/input/js0", O_RDONLY);
-   fcntl(fd_joy, F_SETFL, O_NONBLOCK);
+   //fcntl(fd_joy, F_SETFL, O_NONBLOCK);
    if (fd_joy == -1){
-      puts("ERROR_Missing.JS\n");
+      puts("ERROR_Missing.JS");
       return -1;
    }
  
-   puts("AllSettings_done\n");
+   puts("AllSettings_done");
    
    while(1){
  
-   if (read(fd_joy,&event,sizeof(struct js_event)) < sizeof(struct js_event))continue;
+     if (read(fd_joy,&event,sizeof(struct js_event)) < sizeof(struct js_event))continue;
    
 
-   switch (event.type & ~JS_EVENT_INIT){
-      case JS_EVENT_AXIS:
-         
-         buf  = (stickData[event.number] = event.value);
-         if (event.value < 0){
-            stickData[event.number] = 'L';
-            puts("L\n");
+     switch (event.type & ~JS_EVENT_INIT){
+     
+     case JS_EVENT_AXIS:
+
+       switch (event.number) {
+       case 0:
+         if (event.value < 0) {
+           buf = 'L';
+           puts("L");
+         } else if (event.value == 0 ) {
+           buf = 'K';
+           puts("K");
+         } else {
+           buf = 'R';
+           puts("R");
          }
-         if (event.value == 0){
-            stickData[event.number] = 'K';
-            puts("K\n");
-         }
-         if (event.value > 0){
-            stickData[event.number] = 'R';
-            puts("R\n");
-         }
-         buf = stickData[event.number];
-         puts("FRONTDATA__SEND\n");
          sendto(fd_sock, &buf, sizeof(char), 0, (struct sockaddr *) &svaddr, len);
+
+         puts("FRONTDATA__SEND");
          break;
-         
-      case JS_EVENT_BUTTON:
-         
+       }
+
+       break;
+
+     case JS_EVENT_BUTTON:
+
+       switch (event.value){
+       case 1:
          switch (event.number){
-            case 8:
-               buttonData[event.number] = event.value;
-               puts("___FWD___\n");
-               break;
-            case 9:
-               buttonData[event.number] = event.value;
-               puts("_____BACK_____\n");
-               break;
-            default:
-               puts("__KEEP__");
-               
+         case 8:
+           buf = 'F';
+           puts("F");
+           break;
+         case 9:
+           buf = 'B';
+           puts("B");
+           break;
          }
-         buf = buttonData[event.number];
-         puts("REARDATSA__SEND");
-         sendto(fd_sock, &buf, sizeof(char), 0, (struct sockaddr *) &svaddr, len);
          break;
-  }
+
+       case 0:
+         buf = 'N';
+         puts("N");
+         break;
+
+       default:
+         puts("\n\n\n_____!!ERROR!!ofBUTTTON_____\n\n\n");
+       }
+         
+       sendto(fd_sock, &buf, sizeof(char), 0, (struct sockaddr *) &svaddr, len);
+//         switch (event.number){
+//            case 8:
+//               switch (event.value){
+//
+//               puts("___FWD___");
+//               break;
+//            case 9:
+//               buttonData[event.number] = ;
+//               puts("_____BACK_____");
+//               break;
+//            default:
+//               puts("__KEEP__");
+//               
+//         }
+       break;
+     }
 
    }//while
 }//main
@@ -150,7 +175,7 @@ int main (int argc, char *argv[]){
 
 
 //   if ((event.type == JS_EVENT_AXIS) && (event.number == 0)){
-//      puts("JS_resolved\n");
+//      puts("JS_resolved");
 //   }
 //   if (event.value < 0){
 //       buf = 'L';puts("L");
@@ -161,9 +186,9 @@ int main (int argc, char *argv[]){
 //      else{
 //        buf = 'R';puts("R");
 //      }
-//   puts("buf_done\n");
+//   puts("buf_done");
 //   sendto(fd_sock, &buf, sizeof(char), 0, (struct sockaddr *) &svaddr, len);
-//   puts("dataFront_send\n");
+//   puts("dataFront_send");
 //   
 //    if ((event.type == JS_EVENT_BUTTON)){
 //           if (buttonData[8] == buttonData[9])buf = 'N';puts("N");
@@ -171,7 +196,7 @@ int main (int argc, char *argv[]){
 //           if (buttonData[9] == 1)buf = 'F';puts("F");
 //        }
 //   sendto(fd_sock, &buf, sizeof(char), 0, (struct sockaddr *) &svaddr, len);
-//   puts("dataRear_send\n");
+//   puts("dataRear_send");
 //
 
 
